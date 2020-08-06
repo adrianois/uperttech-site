13bcf3fb08d4425fa6892e51515ea06f53c3099b
Lucas Barbosa <llbarbosasc@gmail.com>
2020-08-06 08:54:08 -0400

    start: Inicializando projeto com Webpack e Babel

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GlobalStyle from './styles/GlobalStyle'
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  )
+}
